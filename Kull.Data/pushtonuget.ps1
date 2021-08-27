a585f78fa0eb7ba7737b2b03f995b5218cf810a2
Adrian Ehrsam <ehrsam@kull.ch>
2021-08-27 13:39:22 +0200

    better push script

--- Kull.Data/pushtonuget.ps1
+++ Kull.Data/pushtonuget.ps1
@@ -1,5 +1,5 @@
+Get-ChildItem "$PSScriptRoot/bin/Release" | Remove-Item -Recurse
 dotnet build --configuration Release
-
 Get-ChildItem "$PSScriptRoot/bin/Release" *.nupkg | ForEach-Object {
 	dotnet nuget push $_.FullName --source https://api.nuget.org/v3/index.json --api-key $env:Nuget_API_KEY
 }
