162fe27108e0ffd90a048a0fb06153e6cea6096b
Adrian Ehrsam <ehrsam@kull.ch>
2021-09-23 14:27:31 +0200

    tests for objectname

---
+++ Kull.Data.Test/DBObjectNameTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Kull.Data.Test
+{
+    [TestClass]
+    public class DBObjectNameTest
+    {
+        [TestMethod]
+        public void TestKeyword()
+        {
+            DBObjectName dBObjectName = new DBObjectName("dbo", "SelEct");
+            Assert.AreEqual(dBObjectName.ToString(false, true), "dbo.\"SelEct\"");
+        }
+
+        [TestMethod]
+        public void TestKeyword2()
+        {
+            DBObjectName dBObjectName = "dbo.SelEct";
+            Assert.AreEqual(dBObjectName.ToString(false, true), "dbo.\"SelEct\"");
+        }
+
+        [TestMethod]
+        public void TestNormal()
+        {
+            DBObjectName dBObjectName = new DBObjectName("dbo", "SelEct_Things");
+            Assert.AreEqual(dBObjectName.ToString(false, true), "dbo.SelEct_Things");
+        }
+
+        [TestMethod]
+        public void TestSpace()
+        {
+            DBObjectName dBObjectName = new DBObjectName("dbo", "taes saf");
+            Assert.AreEqual(dBObjectName.ToString(false, true), "dbo.\"taes saf\"");
+        }
+    }
+}
